8798d5ee207457681ddd39c45b14437023c3949d
Dream023 <61343046+Dream023@users.noreply.github.com>
2022-11-12 14:56:01 +0700

    Update Map
    -Add object for mostly puzzle

--- Assets/Scripts/Player_AnimetionController.cs
+++ Assets/Scripts/Player_AnimetionController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Player_AnimetionController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Animator playeranimator;
+    
     void Start()
     {
-        
+        playeranimator = this.gameObject.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        playeranimationcontroll();
+    }
+
+    void playeranimationcontroll()
+    {
+        if(Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.S)||Input.GetKeyDown(KeyCode.D))
+        {
+            playeranimator.SetBool("IsIdle",false);
+            playeranimator.SetBool("IsWalk",true);
+            playeranimator.SetBool("IsRun",false);
+        }
+        else if(Input.GetKeyUp(KeyCode.A)||Input.GetKeyUp(KeyCode.W)||Input.GetKeyUp(KeyCode.S)||Input.GetKeyUp(KeyCode.D))
+        {
+            playeranimator.SetBool("IsIdle",true);
+            playeranimator.SetBool("IsWalk",false);
+            playeranimator.SetBool("IsRun",false);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftShift)||Input.GetKeyDown(KeyCode.RightShift))
+        {
+            playeranimator.SetBool("IsIdle",false);
+            playeranimator.SetBool("IsWalk",false);
+            playeranimator.SetBool("IsRun",true);
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift)||Input.GetKeyUp(KeyCode.RightShift))
+        {
+            playeranimator.SetBool("IsIdle",false);
+            playeranimator.SetBool("IsWalk",true);
+            playeranimator.SetBool("IsRun",false);
+        }
     }
 }
