95c98a24222d33eddfb8bae431ba1a7f437ead1d
Promthep <promthep.panrat@gmail.com>
2022-12-03 21:55:42 +0700

    Cutscene + TutorialLv1
    Cutscene + TutorialLv1

--- Assets/Scripts/Puzzle/MathPuzzle/MathPuzzleController.cs
+++ Assets/Scripts/Puzzle/MathPuzzle/MathPuzzleController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Playables;
+using System.Linq;
+
 
 public class MathPuzzleController : MonoBehaviour
 {
@@ -19,11 +22,14 @@ public class MathPuzzleController : MonoBehaviour
     public GameObject NoComplete;
     public GameObject HaveBeenComplete;
 
+    public PlayableDirector cutseen1;
+    private List<DisablePlayerControl> DisAblePlayerlist;
     private void Start()
     {
        // rewardPrefab.GetComponent<Renderer>().material.color = Color.red;
         NoComplete.SetActive(true);
         HaveBeenComplete.SetActive(false);
+        DisAblePlayerlist = FindObjectsOfType<DisablePlayerControl>().ToList();
     }
     
     public void CheckAnswer()
@@ -40,6 +46,12 @@ public class MathPuzzleController : MonoBehaviour
         // rewardPrefab.GetComponent<Renderer>().material.color = Color.green;
         NoComplete.SetActive(false);
         HaveBeenComplete.SetActive(true);
+        foreach (var player in DisAblePlayerlist)
+        {
+            player.isDisable = true;
+        }
+        cutseen1.Play();
+
     }
 
 }
